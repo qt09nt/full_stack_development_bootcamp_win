3a6029a37617b49f518104b4ce6a3b2f6a717bd1
Queenie <qt09nt@gmail.com>
2024-03-25 22:48:01 -0600

    angular template forms

--- angular_http_service/src/app/app.component.ts
+++ angular_http_service/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeesServiceService } from './employees-service.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -19,7 +20,14 @@ export class AppComponent {
 
   constructor(){
     this.employeeService.getEmployees().subscribe((result:any) => {
+      this.employees = result.employees;
       console.log(result);
     })
+
   }
 }
+//     this.employeeService.getEmployeesById().subscribe((result:any) => {
+//       console.log('Single Employee == ', result);
+//     })
+//   }
+// }
--- angular_http_service/src/app/employees-service.service.ts
+++ angular_http_service/src/app/employees-service.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
@@ -13,7 +13,19 @@ export class EmployeesServiceService {
     const headers = new HttpHeaders({'Content-Type': 'application/json', 'Accept': 'application/json'})
     const options = {headers: headers};
   
-    return this.http.get('url', options);
+    return this.http.get('http://localhost:30000/employees/', options);
+  }
+
+  //create a service to get employeesbyid
+  //change the url to same as your nodejs application to get employeeid
+
+  export class EmployeesServiceById {
+
+    http:HttpClient = inject(HttpClient);
+
+    getEmployees() {
+      
+    }
   }
 
   constructor() { }
