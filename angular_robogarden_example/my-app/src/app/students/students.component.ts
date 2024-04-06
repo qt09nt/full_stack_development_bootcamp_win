eabeb1227376330bf7842a0e46cc04e86184fbbf
Queenie <qt09nt@gmail.com>
2024-04-06 17:16:30 -0600

    import saved data and display result

--- angular_robogarden_example/my-app/src/app/students/students.component.ts
+++ angular_robogarden_example/my-app/src/app/students/students.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { myStudents } from '../student-data'; //import the array of students
 import { NgFor } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-students',
   standalone: true,
-  imports: [],
+  imports: [NgFor, CommonModule],
   templateUrl: './students.component.html',
   styleUrl: './students.component.css'
 })
