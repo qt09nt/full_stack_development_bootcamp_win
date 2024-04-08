784668c48182ca2ac44b0b712f2c2152c860e1e4
Queenie <qt09nt@gmail.com>
2024-04-07 22:57:51 -0600

    angular array of objects 2

---
+++ angular_array_of_objects_2/src/app/example/example.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BrowserModule } from '@angular/platform-browser';
+
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class ExampleModule { }
+
+
+
+
