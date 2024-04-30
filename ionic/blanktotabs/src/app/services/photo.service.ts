50f2f75d46bbeb062e640b84c1e24aa3ab0ff136
Queenie <qt09nt@gmail.com>
2024-04-29 23:57:45 -0600

    camera in ionic blank tabs project

--- ionic/blanktotabs/src/app/services/photo.service.ts
+++ ionic/blanktotabs/src/app/services/photo.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
+import { UserPhoto } from '../user-photo';
 // import { Preferences } from '@capacitor/preferences';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +12,20 @@ export class PhotoService {
 
   constructor() { }
 
+  //public photos = [];
+  //explicitly defining what the keys going to be:
+  public photos: UserPhoto[] = [];
+
   public async addNewToGallery(){
     const capturedPhoto = await Camera.getPhoto({
    
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
+  })
+
+  this.photos.unshift({
+    filepath:"",
+    webviewPath:capturedPhoto.webPath!
   })
 }}
